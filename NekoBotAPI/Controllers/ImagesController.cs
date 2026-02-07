b19ba23daa5e6dd496cf0b6d052bf74f427c90ab
KoriTime <koritime@pm.me>
2026-02-07 14:22:29 -0400

    The start to a API, removing un-needed scripts

---
+++ NekoBotAPI/Controllers/ImagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NekoBotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+	{
+		[HttpGet]
+		public Task<string> Images()
+		{
+			return Task.FromResult("Random BS Blab");
+		}
+	}
+}
--- NekoBotAPI/Program.cs
+++ NekoBotAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using NekoBotV1.Core;
+using NekoBotV1.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var settings = await Settings.LoadAsync();
+if (settings is null)
+{
+	Console.WriteLine("All you nyans are mine! (config.json created!)");
+	return;
+}
+
+builder.Services.AddDbContext<DatabaseContext>(opt => {
+	 var connectionString = settings.ConnectionString(out DatabaseProvider provider);
+
+	 switch (provider)
+	 {
+		 case DatabaseProvider.MySQL:
+			 opt.UseMySQL(connectionString);
+			 break;
+		 case DatabaseProvider.PostgreSQL:
+			 opt.UseNpgsql(connectionString);
+			 break;
+		 default:
+			 throw new ArgumentException("Provider can only be \"mysql\" or \"pgsql\"!");
+	 }
+	 opt.EnableSensitiveDataLogging()
+		 .UseLazyLoadingProxies();
+ }, ServiceLifetime.Transient);
 
 var app = builder.Build();
 
