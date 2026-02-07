a4cae64316e721cf7a7e3d71e723c7470b91200c
KoriTime <koritime@pm.me>
2026-02-07 13:57:11 -0400

    Add project files.

---
+++ NekoBotAPI/Controllers/ActionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NekoBotV1.API.Web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ActionsController : ControllerBase
+	{
+		[HttpGet]
+		public Task<string> Actions()
+		{
+			return Task.FromResult("Random BS");
+		}
+	}
+}
